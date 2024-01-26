61fc405640b1ed29dc0a7bd4b9a1e19c3a68044e
KostasZour <konstantinos.zouraikat@students.iaac.net>
2024-01-26 11:45:32 +0100

    Kostas 21.01 morning

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,14 @@
 <script>
 import { ref } from "vue" // you need this to use ref()
 
+let count = ref(0);
+let scriptName = ref ("addTwoNumbers.gh")
+let name = ref("Kostas")
+
+function increment() {{ 
+count.value++;
+
+}}
 
 </script>
 
@@ -9,6 +17,30 @@ import { ref } from "vue" // you need this to use ref()
 
 <!-- the template is where the html code goes -->
 <template>
+   
+   <div id="navbar" class="container">  
+        <div id="title">Kostas Awesome Website</div>
+        
+        <div id="logo">
+            <img src="./cow.jpg" alt="macad cow">
+
+        </div>
+        
+
+    </div>
+
+    <div id="flex">
+
+        <div id="sidebar" class="container"> Sidebar </div>
+
+        <div id="main" class="container"> Text input </div>
+    </div>
+
+    <div id="content">
+
+        <button @click="increment">Add one more</button>
+        
+    </div>
 
 
 
@@ -18,6 +50,76 @@ import { ref } from "vue" // you need this to use ref()
 
 <!-- style is where the css code goes -->
 <style scoped>
+html{
+    
+    background-color: rgb(59, 59, 59); 
+    color: white;
+}
+
+body{
+
+    margin:0;
+}
+
+div{
+
+    /* put the borders in the inside of container */
+    box-sizing: border-box;
+}
+
+img{
+
+    height: 100%;
+    width: auto;
+
+}
+
+#navbar{
+
+    height: 50px;
+    border-color: red;
+
+
+}
+
+#flex{
+    
+    display: flex;
+    height: calc(100vh - 50px);
+}
+
+#sidebar{
+    
+    width:30%;
+    border-color: blue;
+
+}
+
+#main{
+    width:70%;
+    border-color: green;
+
+}
+
+#title{
+    width: 70%;
+    float:left;
+}
+
+#logo{
+    width: 30%;
+    height: 100%;
+    float:right;
+    text-align: right;
+
+}
+
+.container{
+
+    border-style: dotted;
+    border-width: 1px;
+
+}
 
 
 </style>
